3f88688a00e41df2ca03d0492eb98f2815f6df37
FabiFNA <lukaspvplp7@gmail.com>
2025-12-13 04:00:59 +0100

    Code cleanup; Planned future ideas.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 #include "Kunde.hpp"
 #include "Bank.hpp"
 
+#include <chrono>
+#include <thread>
+
 using namespace std;
 
 int main() {
     cout << "Hello, world!" << endl;
+
+    
+    /*    
+    Als nächstes Run Methode implementieren
+    Befehle wie ADD oder REMOVE oder LIST der kunden implementieren
+    */
+
 
     Bank b("Große Bank");
 
@@ -15,6 +25,19 @@ int main() {
     b.addKunde(&k);
 
     b.outputKunde(1);
+
+    Kunde k2(2, "Max", "max@gmail.com", "+49123456789");
+    b.addKunde(&k2);
+
+    b.outputKunde(2);
+
+    Kunde k3(3, "Max", "max@gmail.com", "+49123456789");
+    b.addKunde(&k3);
+
+    b.outputKunde(3);
+
+
+
 
     return 0;
 }
